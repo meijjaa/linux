502f06de4f5d069e19062834ed2bdb3bcb357055
Frederic Danis <frederic.danis@linux.intel.com>
2015-05-28 11:25:04 +0200

    Bluetooth: btbcm: Add helper functions for UART setup
    Firmware loading may reset the controller UART speed and needs to set
    host UART speed back to init speed.
    
    UART drivers setup is split in 3 parts:
    - btbcm_initialize() resets the controller and returns the firmware
      name based on controller revision and sub_version.
    - btbtcm_patchram() (already existing and public), which takes the
      firmware name as parameter, requests the firmware and loads it to
      the controller.
    - btbcm_finalize() which resets the controller, reads local version
      and checks if the controller address is a default one or not.
    
    Remove firmware name retrieval for UART controllers from
    btbcm_setup_patchram().
    
    Signed-off-by: Frederic Danis <frederic.danis@linux.intel.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/btbcm.h
+++ drivers/bluetooth/btbcm.h
@@ -42,6 +42,9 @@ int btbcm_patchram(struct hci_dev *hdev, const struct firmware *fw);
 int btbcm_setup_patchram(struct hci_dev *hdev);
 int btbcm_setup_apple(struct hci_dev *hdev);
 
+int btbcm_initialize(struct hci_dev *hdev, char *fw_name, size_t len);
+int btbcm_finalize(struct hci_dev *hdev);
+
 #else
 
 static inline int btbcm_check_bdaddr(struct hci_dev *hdev)
@@ -65,6 +68,17 @@ static inline int btbcm_setup_patchram(struct hci_dev *hdev)
 }
 
 static inline int btbcm_setup_apple(struct hci_dev *hdev)
+{
+	return 0;
+}
+
+static inline int btbcm_initialize(struct hci_dev *hdev, char *fw_name,
+				   size_t len)
+{
+	return 0;
+}
+
+static inline int btbcm_finalize(struct hci_dev *hdev)
 {
 	return 0;
 }
