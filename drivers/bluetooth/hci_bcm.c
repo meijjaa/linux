b3829cc4668ceb417083db075496872b8727591e
meijjaa <jjmeijer88@gmail.com>
2016-04-14 18:10:36 +0200

    Cleanup backport of HCI BCM driver

--- drivers/bluetooth/hci_bcm.c
+++ drivers/bluetooth/hci_bcm.c
@@ -79,55 +79,6 @@ struct bcm_data {
 static DEFINE_MUTEX(bcm_device_lock);
 static LIST_HEAD(bcm_device_list);
 
-static int bcm_set_baudrate(struct hci_uart *hu, unsigned int speed)
-{
-	struct hci_dev *hdev = hu->hdev;
-	struct sk_buff *skb;
-	struct bcm_update_uart_baud_rate param;
-
-	if (speed > 3000000) {
-		struct bcm_write_uart_clock_setting clock;
-
-		clock.type = BCM_UART_CLOCK_48MHZ;
-
-		bt_dev_dbg(hdev, "Set Controller clock (%d)", clock.type);
-
-		/* This Broadcom specific command changes the UART's controller
-		 * clock for baud rate > 3000000.
-		 */
-		skb = __hci_cmd_sync(hdev, 0xfc45, 1, &clock, HCI_INIT_TIMEOUT);
-		if (IS_ERR(skb)) {
-			int err = PTR_ERR(skb);
-			bt_dev_err(hdev, "BCM: failed to write clock (%d)",
-				   err);
-			return err;
-		}
-
-		kfree_skb(skb);
-	}
-
-	bt_dev_dbg(hdev, "Set Controller UART speed to %d bit/s", speed);
-
-	param.zero = cpu_to_le16(0);
-	param.baud_rate = cpu_to_le32(speed);
-
-	/* This Broadcom specific command changes the UART's controller baud
-	 * rate.
-	 */
-	skb = __hci_cmd_sync(hdev, 0xfc18, sizeof(param), &param,
-			     HCI_INIT_TIMEOUT);
-	if (IS_ERR(skb)) {
-		int err = PTR_ERR(skb);
-		bt_dev_err(hdev, "BCM: failed to write update baudrate (%d)",
-			   err);
-		return err;
-	}
-
-	kfree_skb(skb);
-
-	return 0;
-}
-
 /* bcm_device_exists should be protected by bcm_device_lock */
 static bool bcm_device_exists(struct bcm_device *device)
 {
